0bc056f3f35e7219fb05ca3c73ea18cd4b90130d
WynneYU-ece <uwwwynne@gmail.com>
2026-04-06 19:11:55 +0800

    feat(key):
    implement key input driver with software debouncing

---
+++ 05_buttoncontrol/user/main.c
@@ -0,0 +1,33 @@
+#include "stm32f10x.h"                  // Device header
+#include "led.h"
+#include "Delay.h"
+#include "key.h"
+
+uint8_t KeyNum;
+
+int main(void)
+{
+
+	LED_Init(GPIOA, GPIO_Pin_0 | GPIO_Pin_2);
+	KEY_Init(GPIOB, GPIO_Pin_0 | GPIO_Pin_10);
+	
+	while(1)
+	{
+	
+		KeyNum = Key_GetNum(GPIOB, GPIO_Pin_10);
+		
+		if(KeyNum == 1)
+		{
+		
+		LED_On(GPIOA, GPIO_Pin_0);
+		
+		}
+		if(KeyNum == 2)
+		{
+		
+		LED_Off(GPIOA, GPIO_Pin_0);
+		
+		}
+	}
+
+}
