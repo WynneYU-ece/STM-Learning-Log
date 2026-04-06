69c0228d0b474401c15acd9072fef12dc22ebadf
WynneYU-ece <uwwwynne@gmail.com>
2026-04-06 13:39:38 +0800

    style：delete some unnecessary comments

--- 04_humming/user/main.c
+++ 04_humming/user/main.c
@@ -9,9 +9,9 @@ int main(void)
 
 	while(1)
 	{
-		//LED_On(GPIOB, GPIO_Pin_12);
-			//Delay_ms(100);
+		LED_On(GPIOB, GPIO_Pin_12);
+			Delay_ms(100);
 		LED_Off(GPIOB, GPIO_Pin_12);
-			//Delay_ms(100); 
+			Delay_ms(100); 
 	}
 }
