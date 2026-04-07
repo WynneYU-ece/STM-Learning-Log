0f9fa9f85345a39875bd0992d9be7163cde910c0
WynneYU-ece <uwwwynne@gmail.com>
2026-04-07 21:39:48 +0800

    feat: implement light-controlled buzzer alarm
    - Add photoresistor (ADC/GPIO) input logic to monitor ambient light levels.
    - Trigger buzzer output when light off.
    - Configure GPIO pins for both the sensor and the buzzer.

---
+++ 06_LSS/hardware/led.h
@@ -0,0 +1,12 @@
+#ifndef __LED_H
+#define __LED_H
+#include "stm32f10x.h"
+
+#define LED_PIN   GPIO_Pin_0
+#define LED_PORT  GPIOA
+
+void LED_Init(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+void LED_On(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+void LED_Off(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+void LED_Turn(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+#endif
---
+++ 06_LSS/user/main.c
@@ -0,0 +1,25 @@
+#include "stm32f10x.h"                  // Device header
+#include "lss.h"
+#include "buzzer.h"
+#include "Delay.h"
+#include "led.h"
+
+int main(void)
+{
+	BUZZER_Init(GPIOB, GPIO_Pin_12);
+	Lss_Init(GPIOB, GPIO_Pin_13);
+	
+	while(1)
+	{
+		//LED_On(GPIOA, GPIO_Pin_0); 
+		
+		if (Lss_Get(GPIOB, GPIO_Pin_13) == 1)
+			BUZZER_On(GPIOB, GPIO_Pin_12);
+		else
+			BUZZER_Off(GPIOB, GPIO_Pin_12);
+		
+			
+	
+	}
+
+}
