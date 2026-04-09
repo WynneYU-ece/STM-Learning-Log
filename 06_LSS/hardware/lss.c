70b7a0c8b5c7c9982ca1283409d0b2d12a42bdd3
WynneYU-ece <uwwwynne@gmail.com>
2026-04-09 23:11:00 +0800

    refactor:remove some interminable code

--- 06_LSS/hardware/lss.c
+++ 06_LSS/hardware/lss.c
@@ -14,8 +14,6 @@ if (GPIOx == GPIOA) RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin;          
 	GPIO_Init(GPIOx, &GPIO_InitStructure);
 	
-	//GPIO_SetBits(GPIOx, GPIO_Pin);
-
 }
 
 
