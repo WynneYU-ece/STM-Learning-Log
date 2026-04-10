4a6441e870fff6ed05ec9987294d083d262cb467
WynneYU-ece <uwwwynne@gmail.com>
2026-04-10 23:36:25 +0800

    add some comment

--- 03_LED_Water/user/main.c
+++ 03_LED_Water/user/main.c
@@ -6,12 +6,12 @@ uint16_t LED_Pins[] = {GPIO_Pin_0, GPIO_Pin_1, GPIO_Pin_2, GPIO_Pin_3};
 
 int main(void)
 {	
-	for(int i=0; i<4; i++)
+	for(int i=0; i<4; i++) //initialize 4 led
 			LED_Init(GPIOA, LED_Pins[i]);
 	
 	while(1)
 	{
-		for(int i=0; i<4; i++)
+		for(int i=0; i<4; i++) //blinking in range.
 		{	LED_On(GPIOA, LED_Pins[i]);
 				Delay_ms(100);
 			LED_Off(GPIOA, LED_Pins[i]);
