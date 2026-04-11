e0dcf1c22bf2c186150c60e1429670f54a80f6e0
WynneYU-ece <uwwwynne@gmail.com>
2026-04-11 21:46:18 +0800

    remove some interminable code

--- 07_OLED/user/main.c
+++ 07_OLED/user/main.c
@@ -1,19 +1,12 @@
 #include "stm32f10x.h"                  // Device header
 #include "OLED.h"
-#include "Delay.h"
-#include "led.h"
-#include "ir.h"
 int main(void)
 {   
-	
-	IR_Init(GPIOB, GPIO_Pin_14);
 	OLED_Init();
 	OLED_ShowChar(1, 1, 'S');
 	while(1)
 	{
-		IR_On(GPIOB, GPIO_Pin_14);
-		LED_On(GPIOB, GPIO_Pin_4);
-		LED_On(GPIOB, GPIO_Pin_3);
+		
 		
 	}
 }
